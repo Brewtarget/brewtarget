e99c27e3c1fe3a42a2b7707f1f38d2bd6694fba1
Philip Greggory Lee <rocketman768@gmail.com>
2011-03-14 20:41:35 +0000

    Added full Czech support in the options dialog.

--- src/OptionDialog.cpp
+++ src/OptionDialog.cpp
@@ -48,6 +48,7 @@ OptionDialog::OptionDialog(QWidget* parent)
    colorUnitGroup = new QButtonGroup(this);
 
    // Set up language map.
+   languageToButtonMap["cs"] = pushButton_cs;
    languageToButtonMap["de"] = pushButton_de;
    languageToButtonMap["en"] = pushButton_en;
    languageToButtonMap["es"] = pushButton_es;
@@ -69,6 +70,7 @@ OptionDialog::OptionDialog(QWidget* parent)
    pushButton_de->setEnabled(false);
 
    // Set up the buttons in languageGroup;
+   languageGroup->addButton(pushButton_cs);
    languageGroup->addButton(pushButton_de);
    languageGroup->addButton(pushButton_en);
    languageGroup->addButton(pushButton_es);
