d3cfd9dd3a56e4e49618e98f883524ad91eb3a9d
mik firestone <mikfire@gmail.com>
2013-02-03 21:48:07 -0500

    Make sure we connect all our signals

--- src/database.cpp
+++ src/database.cpp
@@ -224,7 +224,11 @@ bool Database::load()
    {
       Equipment* e = equipment(*i);
       if( e )
+      {
          connect( e, SIGNAL(changed(QMetaProperty,QVariant)), *i, SLOT(acceptEquipChange(QMetaProperty,QVariant)) );
+         connect( e, SIGNAL(changedBoilSize_l(double)), *i, SLOT(setBoilSize_l(double)));
+         connect( e, SIGNAL(changedBoilTime_min(double)), *i, SLOT(setBoilTime_min(double)));
+      }
       
       QList<Fermentable*> tmpF = fermentables(*i);
       for( j = tmpF.begin(); j != tmpF.end(); j++ )
