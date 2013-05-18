898af08bc95bb73d9f09cad58308738d46ef72e7
Philip G. Lee <rocketman768@gmail.com>
2013-05-18 13:49:27 -0500

    Made a new SRM->RGB conversion formula that is lighter and more consistent with other software. Fixes #1177546.

--- src/recipe.cpp
+++ src/recipe.cpp
@@ -1765,17 +1765,24 @@ void Recipe::recalcGrains_kg()
 void Recipe::recalcSRMColor()
 {
    QColor tmp;
+   /*
    //==========My approximation from a photo and spreadsheet===========
-
    double red = 232.9 * pow( (double)0.93, _color_srm );
    double green = (double)-106.25 * log(_color_srm) + 280.9;
 
    int r = (int)Algorithms::Instance().round(red);
    int g = (int)Algorithms::Instance().round(green);
    int b = 0;
-
+   */
+   
+   // Philip Lee's approximation from a color swatch and curve fitting.
+   int r = 0.5 + (272.098 - 5.80255*_color_srm); if( r > 253.0 ) r = 253.0;
+   int g = 0.5 + (2.41975e2 - 1.3314e1*_color_srm + 1.881895e-1*_color_srm*_color_srm);
+   int b = 0.5 + (179.3 - 28.7*_color_srm);
+   
    r = (r < 0) ? 0 : ((r > 255)? 255 : r);
    g = (g < 0) ? 0 : ((g > 255)? 255 : g);
+   b = (b < 0) ? 0 : ((b > 255)? 255 : b);
    tmp.setRgb( r, g, b );
 
    if ( tmp != _SRMColor )
