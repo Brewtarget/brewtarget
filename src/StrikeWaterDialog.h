81d2b127470e5f71dd26f9afaf8b52765f44b998
Maxime Lavigne <duguigne@gmail.com>
2014-01-20 14:24:22 -0500

    Started working on implementing a Strike Water toolbox.
      * Bug #1109657
    
      As was suggested, in the bug linked to this commit, I started the
      implementation of a strike water toolbox.

---
+++ src/StrikeWaterDialog.h
@@ -0,0 +1,43 @@
+/*
+* PrimingDialog.h is part of Brewtarget, and is Copyright Philip G. Lee
+* (rocketman768@gmail.com), 2009-2013.
+*
+* Brewtarget is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* Brewtarget is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef _STRIKEWATERDIALOG_H
+#define _STRIKEWATERDIALOG_H
+
+#include <QDialog>
+#include <QWidget>
+#include <QButtonGroup>
+#include "ui_strikeWaterDialog.h"
+
+/*!
+ * \class PrimingDialog
+ * \author Philip G. Lee
+ *
+ * \brief Dialog to calculate priming sugar amounts.
+ */
+class StrikeWaterDialog : public QDialog, public Ui::strikeWaterDialog
+{
+   Q_OBJECT
+   public:
+      StrikeWaterDialog(QWidget* parent = 0);
+      ~StrikeWaterDialog();
+      
+   public slots:
+      void calculate();
+};
+
+#endif  _STRIKEWATERDIALOG_H
