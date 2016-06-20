40b1e69ff2e04ad78efa0b85862ca2f7cd5f78c0
rhoob <rhoob@yahoo.com>
2016-06-19 19:59:22 -0700

    Equipment Editor learning.  Added Alcohol calculator.

---
+++ src/AlcoholTool.h
@@ -0,0 +1,78 @@
+/*
+ * AlcoholTool.h is part of Brewtarget and was written by Ryan Hoobler
+ * (rhoob@yahoo.com).  Copyright is granted to Philip G. Lee
+ * (rocketman768@gmail.com), 2016.
+ *
+ * Brewtarget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Brewtarget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ALCOHOLTOOL_H
+#define  _ALCOHOLTOOL_H
+
+class AlcoholTool;
+
+#include <QDialog>
+#include <QWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+#include <QEvent>
+#include <brewtarget.h>
+#include "BtLineEdit.h"
+#include "BtLabel.h"
+
+/*!
+ * \brief Dialog to convert units.
+ * \author Philip G. Lee
+ */
+class AlcoholTool : public QDialog
+{
+   Q_OBJECT
+public:
+
+   AlcoholTool(QWidget* parent=0);
+
+   //! \name Public UI Variables
+   //! @{
+   QPushButton* pushButton_convert;
+   QLabel* label_og;
+   BtLineEdit* lineEdit_og;
+   QLabel* label_fg;
+   BtLineEdit* lineEdit_fg;
+ //  QLabel* inputTempLabel;
+ //  BtTemperatureEdit* lineEdit_inputTemp;
+
+   QLabel *label_result;
+   QLineEdit *lineEdit_result;
+   //! @}
+
+public slots:
+   void convert();
+
+protected:
+
+   virtual void changeEvent(QEvent* event)
+   {
+      if(event->type() == QEvent::LanguageChange)
+         retranslateUi();
+      QDialog::changeEvent(event);
+   }
+
+private:
+
+   void doLayout();
+   void retranslateUi();
+};
+
+#endif
