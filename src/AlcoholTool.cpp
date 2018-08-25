b1419a8f6b47ed404dfeab4d486aedec72a005ef
rhoob <rhoob@yahoo.com>
2018-08-25 16:19:29 -0700

    Corrected copyright

--- src/AlcoholTool.cpp
+++ src/AlcoholTool.cpp
@@ -1,7 +1,7 @@
-/*
- * AlcoholTool.cpp is part of Brewtarget and was written by Ryan Hoobler
- * (rhoob@yahoo.com).  Copyright is granted to Philip G. Lee
- * (rocketman768@gmail.com), 2016.
+ /*
+ * AlcoholTool.cpp is part of Brewtarget, and is Copyright the following
+ * authors 2009-2018
+ * - Ryan Hoobler <rhoob@yahoo.com>
  *
  * Brewtarget is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
