e33a18c51154c103808c93d7eecdc7a53dff05a1
Idar Lund <idarlund@gmail.com>
2019-07-30 17:51:51 +0200

    Update OptionDialog.cpp

--- src/OptionDialog.cpp
+++ src/OptionDialog.cpp
@@ -88,7 +88,7 @@ OptionDialog::OptionDialog(QWidget* parent)
    langIcons <<
       /*ca*/ QIcon(":images/flagCatalonia.svg") <<
       /*cs*/ QIcon(":images/flagCzech.svg") <<
-      /*da*/ QIcon() <<
+      /*da*/ QIcon(":images/flagDenmark.svg") <<
       /*de*/ QIcon(":images/flagGermany.svg") <<
       /*el*/ QIcon(":images/flagGreece.svg") <<
       /*en*/ QIcon(":images/flagUK.svg") <<
@@ -106,7 +106,7 @@ OptionDialog::OptionDialog(QWidget* parent)
       /*pt*/ QIcon(":images/flagBrazil.svg") <<
       /*ru*/ QIcon(":images/flagRussia.svg") <<
       /*sr*/ QIcon() <<
-      /*sv*/ QIcon() <<
+      /*sv*/ QIcon(":images/flagSweden.svg") <<
       /*tr*/ QIcon() <<
       /*zh*/ QIcon(":images/flagChina.svg");
    // Set icons.
