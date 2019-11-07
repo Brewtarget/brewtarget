be2fc23c2d28d8e7014031c079d0040ff34edfd6
Chris Speck <cgspeck@gmail.com>
2019-11-07 20:14:04 +1100

    Guacgui docker image (#586)

--- docker/ubuntu1804/build.sh
+++ docker/ubuntu1804/build.sh
@@ -1,6 +1,15 @@
 #! /bin/bash -ex
 SRC_PATH=/app/brewtarget
 BUILD_PATH=/app/build
+
+echo -e "\nApplying patches\n"
+patch=/app/brewtarget/docker/ubuntu1804/patches/0001-Revert-CMakelist-change-because-2.3.4-is-not-availab.patch
+cd $SRC_PATH
+git apply --stat $patch
+git apply --check $patch
+# git am < $patch  # <- am would do an apply & commit
+git apply $patch
+echo -e "\nStarting build...\n"
 [ -d $BUILD_PATH ] && rm -rf $BUILD_PATH
 mkdir $BUILD_PATH
 cd $BUILD_PATH
---
+++ release-scripts/docker-build-and-release-guacgui.sh
@@ -0,0 +1,40 @@
+#! /bin/bash -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/common-vars
+
+if [[ "$#" -eq 1 ]]; then
+  TAG_NAME=$1
+  echo "Overriding tag, using ${TAG_NAME}"
+fi
+
+BUILD_PATH=/app/build
+
+if [[ "$TRAVIS" == "true" ]]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
+
+echo "Building for ${TAG_NAME}"
+
+tag="latest"
+
+docker build \
+  -t cgspeck/brewtarget-guacgui:$tag \
+  -f Dockerfile-guacgui \
+  --build-arg BUILD_DATE=$(date -u +%s) \
+  --build-arg VERSION=$TAG_NAME \
+  .
+
+if [[ "$TRAVIS" == "true" ]]; then
+  echo -e "\nPushing new docker images"
+  docker push cgspeck/brewtarget-guacgui:$tag
+  echo -e "\nUpdating Docker Hub Readme"
+  sed "s/%TAG_NAME%/${TAG_NAME}/" $PWD/docker/guacgui/README.md | sed  "s/%SHORT_HASH%/${SHORT_HASH}/" > $PWD/docker/guacgui/README.md.tmp
+  mv $PWD/docker/guacgui/README.md.tmp $PWD/docker/guacgui/README.md
+  docker run -v $PWD/docker/guacgui:/workspace \
+    -e DOCKERHUB_USERNAME=$DOCKER_USERNAME \
+    -e DOCKERHUB_PASSWORD=$DOCKER_PASSWORD \
+    -e DOCKERHUB_REPOSITORY='cgspeck/brewtarget-guacgui' \
+    -e README_FILEPATH='/workspace/README.md' \
+    peterevans/dockerhub-description:2.1.0
+fi
