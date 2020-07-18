879ed78cd600e0e9923ef375f1631448a1f85a30
Chris Speck <cgspeck@gmail.com>
2020-07-18 11:57:08 +1000

    Update Github Releases version

--- release-scripts/push-ubuntu-binaries.sh
+++ release-scripts/push-ubuntu-binaries.sh
@@ -26,7 +26,7 @@ echo -e "\nDownloading github-releases tool"
 tmp_dir=$(mktemp -d)
 github_release_archive=$tmp_dir/github-release.bz2
 github_release_path=$tmp_dir/github-release
-wget "https://github.com/meterup/github-release/releases/download/v0.7.5/linux-amd64-github-release.bz2" -O $github_release_archive
+wget $GITHUB_RELEASES_URL -O $github_release_archive
 bunzip2 $github_release_archive
 chmod +x $github_release_path
 echo -e "\nUploading binaries to Github"
