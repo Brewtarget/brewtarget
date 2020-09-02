ae5198b9f164906d836ae0c52b3978a02d78aaeb
mattias <mattias@kejsarsten.com>
2020-09-02 13:42:41 +0300

    Refaktoring and adding error handling to Python script: add-to-default-db.py

--- scripts/add-to-default-db.py
+++ scripts/add-to-default-db.py
@@ -4,58 +4,49 @@ import sys
 import json
 import sqlite3
 
-def addIng(cursor, x, tableName):
-   setClause = ''
-   for c, v in x.items():
-      setClause = setClause + '{0} = "{1}", '.format(c,v)
-   setClause = setClause[:-2]
+class DbContext:
+   def __init__(self, filename):
+      self.dbconn = sqlite3.connect(filename)
+
+   def __enter__(self):
+      return self
+
+   def __exit__(self, type, value, traceback):
+      if type:
+         print("Error occured while handling data, Rolling back any changes to the database:\n '{0}':'{1}'\n{2}".format(type, value, traceback))
+         self.dbconn.rollback()
+      else:
+         self.dbconn.commit()
+      self.dbconn.close()
+      return True
    
-   cursor.execute('INSERT INTO {0} DEFAULT VALUES'.format(tableName))
-   newId = cursor.lastrowid
+   def addBatch(self, jsondict, table):
+      for row in jsondict.get(table, []):
+         self.addIng(row, table)
 
-   cursor.execute('UPDATE {0} SET {1} WHERE id = {2}'.format(tableName, setClause, newId))
-
-   cursor.execute('INSERT INTO bt_{0} DEFAULT VALUES'.format(tableName))
-   newBtId = cursor.lastrowid
-   cursor.execute(
-      'UPDATE bt_{0} '.format(tableName) +
-      'SET {0}_id = '.format(tableName) + str(newId) + ' ' +
-      'WHERE id = ' + str(newBtId)
-   )
-
-def addFerm(cursor, x):
-   addIng(cursor, x, "fermentable")
-
-def addHop(cursor, x):
-   addIng(cursor, x, "hop")
-
-def addMisc(cursor, x):
-   addIng(cursor, x, "misc")
-
-def addYeast(cursor, x):
-   addIng(cursor, x, "yeast")
+   def addIng(self, x, tableName):
+      cursor = self.dbconn.cursor()
+      setClause = ''
+      for c, v in x.items():
+         setClause = setClause + '{0} = "{1}", '.format(c,v)
+      setClause = setClause[:-2]
+      cursor.execute('INSERT INTO {0} DEFAULT VALUES'.format(tableName))
+      newId = cursor.lastrowid
+      cursor.execute('UPDATE {0} SET {1} WHERE id = {2}'.format(tableName, setClause, newId))
+      cursor.execute('INSERT INTO bt_{0} DEFAULT VALUES'.format(tableName))
+      newBtId = cursor.lastrowid
+      cursor.execute(
+         'UPDATE bt_{0} '.format(tableName) +
+         'SET {0}_id = '.format(tableName) + str(newId) + ' ' +
+         'WHERE id = ' + str(newBtId)
+         )
 
 if __name__ == "__main__" :
-   
    jsonFilename = sys.argv[1]
    databaseFilename = '../data/default_db.sqlite'
-   dbConn = sqlite3.connect(databaseFilename)
-   c = dbConn.cursor()
-   
-   with open(jsonFilename) as jsonFile:
+   with open(jsonFilename) as jsonFile, DbContext(databaseFilename) as c:
       bigDict = json.load(jsonFile)
-      ferm = bigDict.get('fermentable', [])
-      for f in ferm:
-         addFermentable(c,f)
-      hop = bigDict.get('hop', [])
-      for h in hop:
-         addHop(c,h)
-      misc = bigDict.get('misc', [])
-      for m in misc:
-         addMisc(c,m)
-      yeast = bigDict.get('yeast', [])
-      for y in yeast:
-         addYeast(c, y)
-
-   dbConn.commit()
-   dbConn.close()
+      c.addBatch(bigDict, 'fermentable')
+      c.addBatch(bigDict, 'hop')
+      c.addBatch(bigDict, 'misc')
+      c.addBatch(bigDict, 'yeast')
